bf406b495a98925fe432d51bab2a98b4bfba8e18
EleazarrGamezD <eleazar.gamezd@gmail.com>
2026-09-14 17:59:33 -0500

    Refactor chat application: update title, enhance UI with input fields, and improve WebSocket connection handling

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,36 @@ import { connectToServer } from './socket-client.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h2>WebSocket - Client</h2>
+    <div id="chat-id"></div>
+    <div class="form-group">   
+      <input id="jwt-tokenUser" placeholder="Json Web Token User" />
+      <input id="userToConnectId" placeholder="User that you want to connect" />
+      <input id="transaction-id" placeholder="transaction Id" />
+    </div>
 
-    <input id="jwt-token" placeholder="Json Web Token" />
     <button id="btn-connect">Connect</button>
     <br/>
-    <spam id = "server-status">offline</spam>
+    <span id="server-status">offline</span>
     <ul id="clients-ul"></ul>
     <form id="message-form">
-    <input placeholder="message" id="message-input"/>
+      <input placeholder="message" id="message-input"/>
     </form>
     <h3>Mensajes</h3>
     <ul id="messages-ul"></ul>
-    </div>
-`
+  </div>
+`;
+
 
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 // connectToServer()
-const jwtToken = document.querySelector<HTMLInputElement>('#jwt-token')!
+const jwtToken = document.querySelector<HTMLInputElement>('#jwt-tokenUser')!
+const userToConnectId = document.querySelector<HTMLInputElement>('#userToConnectId')!
+const transactionId = document.querySelector<HTMLInputElement>('#transaction-id')!
 const btnConnect = document.querySelector<HTMLInputElement>('#btn-connect')!
 
 btnConnect.addEventListener('click', () => {
   if (jwtToken.value.trim().length <= 0) return alert('enter a valid JWT')
-  connectToServer(jwtToken.value)
+  if (userToConnectId.value.trim().length <= 0) return alert('enter a valid userToConnectId')
+  if (transactionId.value.trim().length <= 0) return alert('enter a valid transactionId')  
+  connectToServer(jwtToken.value , userToConnectId.value, transactionId.value)
 })
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,64 +1,178 @@
-import { Manager, Socket } from 'socket.io-client'
+import { Manager, Socket } from 'socket.io-client';
 
-let socket: Socket
-
-export const connectToServer = (token: string) => {
+let socket: Socket;
+let currentChatId: string;
+interface Message {
+    chatId: string;
+    isRead: boolean;
+isReceived: boolean;
+    content: string;
+    createdAt: string;
+    _id: string;
+    sender:{
+        _id: string;
+        firstName: string;
+        lastName: string;
+        email: string;
+    }
+    recipient:{
+        _id: string;
+        firstName: string;
+        lastName: string;
+        email: string;
+    }
+}
+export const connectToServer = (tokenUser: string, otherUserId: string, transactionId: string) => {
+   
     const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
         extraHeaders: {
-            authentication: token
+            Authorization: tokenUser
         }
     });
-    /* socket.removeAllListeners() */
-    socket = manager.socket('/')
-    addListener()
-}
-const addListener = () => {
-
-    const serverStatusLabel = document.querySelector('#server-status')!
-    const clientsUl = document.querySelector('#clients-ul')!
-    const messagesUl = document.querySelector<HTMLAnchorElement>('#messages-ul')!
-    const formMessage = document.querySelector<HTMLFormElement>('#message-form')!
-    const inputMessage = document.querySelector<HTMLInputElement>('#message-input ')!
+    const payload = { otherUserId, transactionId };
+    socket = manager.socket('/comercialChat');
 
     socket.on('connect', () => {
-        serverStatusLabel.innerHTML = 'connected'
-    });
-    socket.on('disconnect', () => {
-        serverStatusLabel.innerHTML = 'disconnected'
+        console.log('connected to server');       
+        socket.emit('joinChat', payload);
     });
 
-    socket.on('clientes-updated', (clients: string[]) => {
+    socket.on('disconnect', () => {
+        console.log('disconnected from server');
+    });
+
+
+    addListeners();
+};
+
+const addListeners = () => {
+    
+    const serverStatusLabel = document.querySelector('#server-status')!;
+    const chatIdLabel = document.querySelector('#chat-id')!
+    const clientsUl = document.querySelector('#clients-ul')!;
+    const messagesUl = document.querySelector<HTMLUListElement>('#messages-ul')!;
+    const formMessage = document.querySelector<HTMLFormElement>('#message-form')!;
+    const inputMessage = document.querySelector<HTMLInputElement>('#message-input')!;
+    const userToConnectId = document.querySelector<HTMLInputElement>('#userToConnectId')!
+    socket.on('connect', () => {
+        serverStatusLabel.innerHTML = 'connected';
+    });
+    
+    socket.on('disconnect', () => {
+        serverStatusLabel.innerHTML = 'disconnected';
+    });
+    socket.on('joinedChat',(chatId:{chatId: string})=>{   
+        console.log(chatId) 
+        const newMessage = ` 
+        <h5>ID del chat</h5>   
+        <strong>${chatId.chatId}</strong>
+           
+        `;
+        currentChatId = chatId.chatId
+        chatIdLabel.innerHTML = newMessage
+    })
+
+  socket.on('clients-updated', (clients: string[]) => {
         let clientsHtml = '';
         clients.forEach(clientId => {
-            clientsHtml += `<li>${clientId}</li>`
+            clientsHtml += `<li>${clientId}</li>`;
         });
-        clientsUl.innerHTML = clientsHtml
+        clientsUl.innerHTML = clientsHtml;
     });
 
     formMessage.addEventListener('submit', (event) => {
-        event.preventDefault()
-        if (inputMessage.value.trim().length <= 0) return
+        event.preventDefault();
+        if (inputMessage.value.trim().length <= 0) return;
+       
+        socket.emit('sendMessage', {
+            content: inputMessage.value, chatId: currentChatId, recipient:userToConnectId.value           
+        });
 
-        socket.emit('message-from-client', {
-            id: 'YO',
-            message: inputMessage.value
-        })
+        inputMessage.value = '';
+    });
 
-        inputMessage.value = ''
-    })
-
-    socket.on('message-from-server', (payload: { fullName: string, message: string }) => {
+    socket.on('restoreMessages', (messages: Message[]) => {   
+        const messagesJson = JSON.stringify(messages, null, 2); // null es para no aplicar un replacer, y 2 es el espacio de indentación    
+        console.log("Messages in JSON format:");
+        console.log(messagesJson);
+        messages.forEach((message: Message) => {
+            console.log(message.recipient._id);
+            const userFullName = message.recipient._id === userToConnectId.value ? `${message.recipient.firstName} ${message.recipient.lastName}` : `${message.sender.firstName} ${message.sender.lastName}`;
+            const formatDate = new Date(message.createdAt).toLocaleString();
+    
+            const receivedStatus = message.isReceived ? '✓' : '';
+            const readStatus = message.isRead ? '✓' : '';
+    
+            const newMessage = `
+                <li id="${message._id}" class="${message.sender._id === userToConnectId.value ? 'my-message' : 'other-message'}">
+                    <strong>${userFullName}</strong>
+                    <span>${message.content}</span>
+                    <span>${formatDate}</span>
+                    <span class="message-status">${receivedStatus}${readStatus}</span>
+                </li>`;
+    
+            messagesUl.insertAdjacentHTML('beforeend', newMessage);   
+       
+        });
+    });
+    
+    
+    socket.on('message', (message: Message) => {
+        console.log(message);
+        const userFullName = `${message.sender.firstName} ${message.sender.lastName}`;
+        const formatDate = new Date(message.createdAt).toLocaleString();
+        
+        // Aquí determinamos si el mensaje es del usuario actual o del otro usuario
+        const isMyMessage = message.sender._id === userToConnectId.value;
+    
+        // Construimos los elementos de estado para isReceived y isRead
+        const receivedStatus = message.isReceived ? '✓' : '';
+        const readStatus = message.isRead ? '✓' : '';
+    
         const newMessage = `
-           <li>
-           <strong>${payload.fullName}</strong>
-           <span>${payload.message}</span>
-           </li>`
-        const li = document.createElement('li')
-        li.innerHTML = newMessage
-        messagesUl.append(li)
-    })
+            <li id="${message._id}" class="${isMyMessage ? 'my-message' : 'other-message'}">
+                <strong>${userFullName}</strong>
+                <span>${message.content}</span>
+                <span>${formatDate}</span>
+                <span class="message-status">${receivedStatus}${readStatus}</span>
+            </li>`;
+        
+        messagesUl.insertAdjacentHTML('beforeend', newMessage);    
+ 
+    });
 
-}
-
-
-
+    socket.on('messageStatusUpdated', (statusUpdate: { messageId: string, isReceived?: boolean, isRead?: boolean }) => {
+        const messageElement = document.getElementById(statusUpdate.messageId);
+        if (messageElement) {
+            const statusSpan = messageElement.querySelector('.message-status');
+            if (statusSpan) {
+                let statusText = '';
+                if (statusUpdate.isRead) {
+                    statusText = '✓✓';
+                } else if (statusUpdate.isReceived) {
+                    statusText = '✓';
+                }
+                statusSpan.textContent = statusText;
+            }
+        }
+    });
+    
+    messagesUl.addEventListener('click', (event) => {
+        const target = event.target as HTMLElement;
+    
+        // Aquí verificamos si el clic fue en un mensaje específico (elemento <li>)
+        if (target.tagName === 'LI') {
+            const messageId = target.getAttribute('id');
+            if (messageId) {
+                // Notificar al servidor que el mensaje ha sido leído
+                socket.emit('messageRead', { messageId: messageId, chatId: currentChatId });
+    
+                // Actualizamos visualmente el estado del mensaje a leído
+                const statusSpan = target.querySelector('.message-status');
+                if (statusSpan) {
+                    statusSpan.textContent = '✓✓';
+                }
+            }
+        }
+    }); 
+};
